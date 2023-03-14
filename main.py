f7c3a03495d6970190a1c909c22ae460298bd9ea
DeeKay29 <dkurowski.kontakt@gmail.com>
2023-03-14 12:59:15 +0100

    resolving problem with json

--- main.py
+++ main.py
@@ -8,19 +8,18 @@ from beautifultable import BeautifulTable as table
 
 # Define game parameters
 try:
-    with open('./data/gameRules.json', 'r') as f:
-        gameRules = json.load(f)
-        colHeaders = gameRules.get('headers', {}).get('columns', [])
-        rowHeaders = gameRules.get('headers', {}).get('rows', [])
-        shipNumb = gameRules.get('shipNumb', {})
+    with open('./data/gameParameters.json', 'r') as f:
+        gameParameters = json.load(f)
+        colHeaders = gameParameters.get('boardHeaders', {}).get('columns', [])
+        rowHeaders = gameParameters.get('boardHeaders', {}).get('rows', [])
+        shipNumb = gameParameters.get('shipNumb', {})
+        gameBoardSize = gameParameters.get('gameBoardSize')
 except FileNotFoundError:
     print(colored('Error: ', 'red', attrs=['bold']) + colored('File not found. ', 'red') + 'Please make sure the `boardHeaders.json` file exists.')
     sys.exit()
 except json.JSONDecodeError:
     print(colored('Error: ', 'red', attrs=['bold']) + colored('JSON decoding error. ', 'red') + 'Please check the data in the `boardHeaders.json` file.')
     sys.exit()
-
-gameBoardSize = 10
 
 # Create empty game board
 def createGameBoard(gameBoardSize, colHeaders, rowHeaders):
