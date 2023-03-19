1080e39d5fe6dc29272d6367430f5ec536377443
DeeKay29 <dkurowski.kontakt@gmail.com>
2023-03-19 17:54:42 +0100

    reorganizing code

--- main.py
+++ main.py
@@ -17,23 +17,6 @@ except json.JSONDecodeError:
     print(colored('Error: ', 'red', attrs=['bold']) + colored('JSON decoding error. ', 'red') + 'Please check the data in the `gameRules.json` file.')
     sys.exit()
 
-# Display general rules
-for rule in game_rules['general rules']:
-    print(colored(rule, "blue"))
-
-# Check if user is ready
-while True:
-    is_user_ready = input('Are you ready admiral? (Y / N):')
-    if is_user_ready in ['Y', 'N']:
-        if is_user_ready == 'Y':
-            break
-        elif is_user_ready == 'N':
-            print('We are sorry to hear that. See you soon admiral!')
-            sys.exit()
-        break
-    else:
-        print('Invalid input. Insert one character (E or M or H).')
-
 # Define game parameters
 try:
     with open('./data/gameParameters.json', 'r') as f:
@@ -50,26 +33,45 @@ except json.JSONDecodeError:
     print(colored('Error: ', 'red', attrs=['bold']) + colored('JSON decoding error. ', 'red') + 'Please check the data in the `boardHeaders.json` file.')
     sys.exit()
 
-# Display difficulty level rules
-for rule in game_rules['difficulty level rules']:
-    print(colored(rule, "blue"))
+# Display rules
+def display_rules():
+    # Display general rules
+    for rule in game_rules['general rules']:
+        print(colored(rule, "blue"))
 
-# Get difficulty level from a user
-while True:
-    difficulty_level = input('Choose difficulty level (E / M / H):')
-    if difficulty_level in ['E', 'M', 'H']:
-        if difficulty_level == 'E':
-            turns = game_turns.get('easy')
+    # Check if user is ready
+    while True:
+        is_user_ready = input('Are you ready admiral? (Y / N):')
+        if is_user_ready in ['Y', 'N']:
+            if is_user_ready == 'Y':
+                break
+            elif is_user_ready == 'N':
+                print('We are sorry to hear that. See you soon admiral!')
+                sys.exit()
             break
-        elif difficulty_level == 'M':
-            turns = game_turns.get('medium')
+        else:
+            print('Invalid input. Insert one character (Y / N).')
+
+    # Display difficulty level rules
+    for rule in game_rules['difficulty level rules']:
+        print(colored(rule, "blue"))
+
+    # Get difficulty level from a user
+    while True:
+        difficulty_level = input('Choose difficulty level (E / M / H):')
+        if difficulty_level in ['E', 'M', 'H']:
+            if difficulty_level == 'E':
+                turns = game_turns.get('easy')
+                break
+            elif difficulty_level == 'M':
+                turns = game_turns.get('medium')
+                break
+            elif difficulty_level == 'H':
+                turns = game_turns.get('hard')
+                break
             break
-        elif difficulty_level == 'H':
-            turns = game_turns.get('hard')
-            break
-        break
-    else:
-        print('Invalid input. Insert one character (E or M or H).')
+        else:
+            print('Invalid input. Insert one character (E or M or H).')
 
 # Create empty game board
 def create_game_board(game_board_size, col_headers, row_headers):
@@ -229,6 +231,9 @@ def game(game_turns):
     # Define game starting parameters
     turns = game_turns
     hits = 0
+    
+    # Display rules
+    display_rules()
 
     # Display player board
     print(colored("Let's begin. Here os your board. When you take a shot, we'll mark its position on it.", 'blue'))
