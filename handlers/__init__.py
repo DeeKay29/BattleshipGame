443a3dc8240e6a965d50567fedad3699ecf4d5dc
DeeKay29 <dkurowski.kontakt@gmail.com>
2023-03-22 21:20:12 +0100

    deleting server handler

--- handlers/__init__.py
+++
@@ -1,1 +0,0 @@
-from .create_server import Server
--- handlers/create_server.py
+++
@@ -1,5 +0,0 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from termcolor import colored
-
-class Server(BaseHTTPRequestHandler):
-    pass
