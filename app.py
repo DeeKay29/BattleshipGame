f8c0866bce7ae9aa56ef7041ee03e9327bc155d1
DeeKay29 <dkurowski.kontakt@gmail.com>
2023-03-22 21:22:07 +0100

    restoring the file to its previous state

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import warnings
 from logic import GameLogic
-from handlers import Server
 
 # Ignore warnings
 warnings.filterwarnings("ignore", message="'BeautifulTable.__getitem__' has been deprecated")
 warnings.filterwarnings("ignore", message="'BeautifulTable.__len__' has been deprecated")
 
 if __name__ == "__main__":
-    # GameLogic().start_game()
-    Server().run()
+    GameLogic().start_game()
