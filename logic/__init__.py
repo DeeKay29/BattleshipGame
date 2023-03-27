f07c7dfe24151a36bacb72907f0e7ec4de87c39d
DeeKay29 <dkurowski.kontakt@gmail.com>
2023-03-27 22:48:50 +0200

    generating ships location

--- logic/__init__.py
+++ logic/__init__.py
@@ -1,1 +1,2 @@
 from .board import Board
+from .ship import Ship
--- logic/board.py
+++ logic/board.py
@@ -1,5 +1,7 @@
 import json
 import os
+import random
+from ship import Ship
 
 class Board:
     def __init__(self):
@@ -7,7 +9,6 @@ class Board:
         with open(game_parameters_path) as f:
             game_parameters = json.load(f)
         self.headers = game_parameters["board headers"]
-        self.ships = game_parameters["ships"]
         self.board_size = game_parameters["game board size"]
         self.turns = game_parameters["turns"]
 
@@ -18,3 +19,30 @@ class Board:
                 cell_id = col_header + row_header + str(col_idx + 1)
                 row.append({'id': cell_id, 'ship': None, 'hit': False})
             self.board.append(row)
+
+        self.ships_list = []
+        for ship_type, ship_data in self.ships.items():
+            for i in range(ship_data['quantity']):
+                ship = Ship(ship_type, ship_data["size"])
+                self.place_ship(ship)
+                self.ships_list.append(ship)
+
+    def generate_ships_location(self, ship):
+        size = ship.size
+        while True:
+            direction = random.choice(['horizontal', 'vertical'])
+            
+            if direction == 'horizontal':
+                x = random.randint(0, self.board_size - 1 - size)
+                y = random.randint(0, self.board_size - 1)
+            else:
+                x = random.randint(0, self.board_size - 1)
+                y = random.randint(0, self.board_size - 1 - size)
+
+            return x, y, direction
+
+    def generate_ships_coordinates(self):
+        pass
+
+    def place_ships(self):
+        pass
